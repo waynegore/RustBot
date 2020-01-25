877b645dbedb5638bb953075d226a1e10c2739d8
Aron Gore <arongore@gmail.com>
2020-01-25 11:52:04 -0500

    'testing'

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use serenity::client::Client;
+use serenity::model::channel::Message;
+use serenity::prelude::{EventHandler, Context};
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!")?;
+
+    Ok(())
+}
+
+#[command]
+fn tarkov(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let response = match msg.content as &str {
+        "woods" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d9/Woods_v1.1_lowres.jpg?version=c556f944c9a53d972eb4685911e8f6c1",
+        _ => {}
+    };
+
+    msg.channel_id.say(ctx, response);
+
+    Ok(())
+}
