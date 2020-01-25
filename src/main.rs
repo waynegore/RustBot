59c39929a2f144a78e7b2f2fd1be217bd8b384bf
Aron Gore <arongore@gmail.com>
2020-01-25 12:39:24 -0500

    workin

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use serenity::framework::standard::{
 };
 
 #[group]
-#[commands(ping)]
+#[commands(tarkov)]
 struct General;
 
 use std::env;
@@ -37,21 +37,28 @@ fn main() {
 }
 
 #[command]
-fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!")?;
-
-    Ok(())
-}
-
-#[command]
 fn tarkov(ctx: &mut Context, msg: &Message) -> CommandResult {
-    println!(msg.content);
-    let response = match msg.content.as_ref() {
-        "woods" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d9/Woods_v1.1_lowres.jpg?version=c556f944c9a53d972eb4685911e8f6c1",
-        _ => "Not found"
-    };
+    let split_msg = msg.content.split(' ').collect::<Vec<&str>>();
 
-    msg.reply(ctx, response);
+    let mut response;
+
+    if split_msg.len() > 1 {
+        let location = split_msg[1];
+        response = match location.as_ref() {
+            "factory" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/8/83/Factory_0.8.png?version=91f04c0c3f62388c86e3fbebdd0abcdf",
+            "customs" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c8/Customs_Nuxx_20190106_1.2.png?version=a3b44edf49616eaad2736c6523c977b0",
+            "woods" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d9/Woods_v1.1_lowres.jpg?version=c556f944c9a53d972eb4685911e8f6c1",
+            "shoreline" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/e/e1/Actual_caches_37_map_shoreline.jpg?version=e589dbcf739214d11dde16957b82c817",
+            "interchange" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/06/InterchangeMapLorathor.jpg?version=2880f134180975c58d5c4dcef4327ef4",
+            "lab" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/0b/TheLabMapFull.png?version=8743e690fbd315e114f51540347eca29",
+            "reserve" => "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c0/ReserveMap3d.jpg?version=69dfeba044fd40717fdd61afa6bc82d2",
+            _ => "Not found"
+        };
+        msg.reply(ctx, response);
+    } else {
+        msg.reply(ctx, "Usage: ~tarkov map-name");
+    }
+
 
     Ok(())
 }
